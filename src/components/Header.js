ed1d58c595132ab6066ea12d9e135ad52cd1bb46
hiromuota166 <otahiromu166@gmail.com>
2023-12-22 00:55:23 +0900

    headerにgoogle font設定

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,7 @@ import '../styles/header.css'
 const Header = () => {
   return (
     <div className='header_top'>
-      <nav role="navigation">
-        <div id="menuToggle">
-          <input type="checkbox" />
-          <span></span>
-          <span></span>
-          <span></span>
-          <ul id="menu">
-            <a href=""><li>Home</li></a>
-            <a href=""><li>About</li></a>
-            <a href=""><li>Info</li></a>
-            <a href=""><li>Contact</li></a>
-            <a href=""><li>Show me more</li></a>
-          </ul>
-        </div>
-      </nav>
+      <h1 className='header_h1'>OpenCourt</h1>
     </div>
   )
 }
